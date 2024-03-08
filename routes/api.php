1e04f67ca9e29ed0a02e0de720f41cf8229d5a2e
maartins-dudaa <mmfmecma203@gmail.com>
2024-03-08 10:23:00 -0300

    Exercico 2

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,10 @@ Route::get('receber/nome', function(Request $request){
     $nome = $request->input('nome');
     return $nome;
     });
+
+    
+    Route::get('nome/idade', function(Request $request){
+        $nome = $request->input('nome');
+        $idade = $request->input('idade');
+        return 'Meu nome é ' . $nome . ' tenho ' . $idade . 'anos de idade';
+        });
